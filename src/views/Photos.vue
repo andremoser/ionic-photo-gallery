0958ce8442b3f62a7b8659676e4c6b592d2d0a7e
Andre Moser <moser@celepar.pr.gov.br>
2024-09-02 10:25:00 -0300

    Update Photos.vue

--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -2,14 +2,21 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Photo Gallery</ion-title>
+        <ion-title>
+          Photo Gallery
+        </ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-grid>
+        <div class="ion-text-right ion-margin-end ion-margin-bottom">  
+          <ion-icon :icon="image" class="ion-padding-top ion-padding-end" />          
+          <ion-toggle :checked="false" @ionChange="changeSize()" />
+          <ion-icon :icon="images" class="ion-padding-top ion-padding-start" />
+        </div>
         <ion-row>
-          <ion-col size="6" :key="photo.filepath" v-for="photo in photos">
-            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)"></ion-img>
+          <ion-col :size="size" :key="photo.filepath" v-for="photo in photos">
+            <ion-img :src="photo.webviewPath" @click="showActionSheet(photo)" class="photo"></ion-img>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -24,8 +31,10 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { 
   actionSheetController,
+  IonToggle,
   IonPage,
   IonHeader,
   IonFab,
@@ -39,9 +48,8 @@ import {
   IonCol,
   IonImg,
 } from '@ionic/vue';
-import { camera, trash, close } from 'ionicons/icons';
+import { camera, trash, close, image, images } from 'ionicons/icons';
 import { usePhotoGallery, UserPhoto } from '@/composables/usePhotoGallery';
-
 const { takePhoto, photos, deletePhoto } = usePhotoGallery();
 
 const showActionSheet = async (photo: UserPhoto) => {
@@ -67,4 +75,18 @@ const showActionSheet = async (photo: UserPhoto) => {
   });
   await actionSheet.present();
 }
+
+const size = ref('12');
+const changeSize = () => {
+  size.value === '12' ? size.value = '3' : size.value = '12'; 
+}
+
 </script>
+
+<style>
+
+.photo:active, .photo:hover {
+  outline: solid 5px #1554e9;
+}
+
+</style>
